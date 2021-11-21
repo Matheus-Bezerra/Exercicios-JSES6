247496e23e5f5ccb1a9e3a0df6bb45b3f07bc0d8
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-21 20:02:42 -0300

    Módulo 2 inicializado

---
+++ modulo2/scripts.js
@@ -0,0 +1,7 @@
+/*
+1) Crie um arquivo chamado functions.js com o seguinte conteúdo passado
+*/
+// 1.1 Agora em seu arquivo principal import apenas a classe Usuario renomeando-a para ClasseUsuario e chame a função info()
+import classeUsuario from './functions.js';
+
+console.log(classeUsuario);
