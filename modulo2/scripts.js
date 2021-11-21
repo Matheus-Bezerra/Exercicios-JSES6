657908965a93d035d82189039df2a2a37ee070ef
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-21 20:16:19 -0300

    Exercicios do módulo 2 resolvidos

--- modulo2/scripts.js
+++ modulo2/scripts.js
@@ -4,4 +4,12 @@
 // 1.1 Agora em seu arquivo principal import apenas a classe Usuario renomeando-a para ClasseUsuario e chame a função info()
 import classeUsuario from './functions.js';
 
-console.log(classeUsuario);
+classeUsuario.info();
+
+// 1.2 Em seu arquivo principal importe a variável de idade e exiba a mesma em tela;
+import { idade } from './functions.js';
+console.log(idade);
+
+//1.3 Em seu arquivo principal importe tanto a classe quanto a variável idade e renomeie a variável idade para IdadeUsuario
+import padraoIdade, { idade as idadeUsuario } from './functions.js';
+console.log(idadeUsuario);
